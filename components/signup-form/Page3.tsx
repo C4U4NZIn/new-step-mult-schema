54e6a59b598baeb3b162e01c2b833f753144edfd
C4U4ZIn <cauazindofree1234@gmail.com>
2024-02-10 15:16:42 -0400

    Second Commit

---
+++ components/signup-form/Page3.tsx
@@ -0,0 +1,12 @@
+import { FormStepComponentType } from "./FormTypeStepProps"
+
+
+ const Page3:FormStepComponentType = () => {
+    return (
+    <div>
+
+    </div>
+    )
+ }
+
+ export default Page3;
---
+++ components/signup-form/SignUpStepComponent.tsx
@@ -0,0 +1,17 @@
+import { FormStepComponentType } from "./FormTypeStepProps"
+
+
+
+type Props = {
+    steps : FormStepComponentType[];
+}
+
+
+export default function SignUpStepComponent({steps}:Props){
+    return (
+        <div>
+        
+        
+        </div>
+    )
+}
